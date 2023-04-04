cb7ec0e9662e1859be21f70c60e9d10cdbd484f6
Nick Mathison <114104601+nick-mathison@users.noreply.github.com>
2023-04-04 16:51:01 -0400

    Remove Cloud specific org info

--- simple-provision/versions.tf
+++ simple-provision/versions.tf
@@ -1,12 +1,4 @@
 terraform {
-  cloud {
-    organization = "nick-delphix"
-
-    workspaces {
-      name = "delphix-api-demo"
-    }
-  }
-
   required_providers {
     delphix = {
       source = "delphix-integrations/delphix"
